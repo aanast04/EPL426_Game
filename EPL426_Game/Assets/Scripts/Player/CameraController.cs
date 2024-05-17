8f7a67ea601b94b5ae12de50acf990549d5c67fa
andreou00 <157217334+andreou00@users.noreply.github.com>
2024-05-17 14:04:51 +0300

    Camera Fixed
    Now it follows any player the user choose from the shop

--- EPL426_Game/Assets/Scripts/Player/CameraController.cs
+++ EPL426_Game/Assets/Scripts/Player/CameraController.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform target;
+    private Transform target;
     private Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        target = GameObject.FindGameObjectWithTag("Player").transform;
         offset = transform.position - target.position;
 
     }
